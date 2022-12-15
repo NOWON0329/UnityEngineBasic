c794dce72336324fee1f19ad67feaa1f5a7e3216
NOWON0329 <savdb032945@gmail.com>
2022-12-15 21:41:31 +0900

    Operators

---
+++ CSharp/IfStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IfStatement
+{
+    internal class Program
+    {
+        static bool condition1 = false;
+        static bool condition2 = false;
+        static void Main(string[] args)
+        {
+            if(condition1)
+            {
+                Console.WriteLine("조건1이 참이다");
+            }
+            else if(condition2)
+            {
+                Console.WriteLine("조건1이 거짓이고 조건2가 참이다");
+            }
+            else
+            {
+                Console.WriteLine("조건1, 2 모두 거짓이다");
+            }
+        }
+    }
+}
