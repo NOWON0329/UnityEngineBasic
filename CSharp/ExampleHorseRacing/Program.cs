89f2c252e6776db4dcd174892a1de796ca086855
NOWON0329 <savdb032945@gmail.com>
2022-12-21 21:52:49 +0900

    Structure

--- CSharp/ExampleHorseRacing/Program.cs
+++ CSharp/ExampleHorseRacing/Program.cs
@@ -19,7 +19,7 @@ namespace Example1_HorseRacing
         class Horse
         {
             public string Name;
-            public bool IsFinished;
+            public bool IsFinished = false;
             public int TotalDistance;
 
             public void Run(int distance)
@@ -35,40 +35,53 @@ namespace Example1_HorseRacing
 
         static void Main(string[] args)
         {
-            bool gameFinished = false;
+            int j = 0;
+            int sec = 0;
             Horse[] horse = new Horse[5];
             string[] name = new string[5];
 
             // 말 다섯마리 만들고 
             // 이름 다 설정 해줌 (이름 패턴은 경주마1, 경주마2, 경주마3 ... )
-
-            while (gameFinished == false)
+            for (int i = 0; i < 5; i++)
             {
+                horse[i] = new Horse();
+                horse[i].Name = "말" + (i + 1);
+            }
+            Console.WriteLine("=================경주 시작================");
+            while (true)
+            {
+                Console.WriteLine("=================경주" + sec + "초 경과================");
                 // 각 말은 초당 10 ~ 20 (정수형) 범위의 거리를 랜덤하게 전진.
                 // 각각의 말은 거리 200에 도달하면 도착해서 더이상 전진하지 않고 
                 // 매초 각 말들이 아직 달리고 있다면 달린 거리를, 도착했다면 도착 상태를 콘솔창에 출력 해줍니다.
                 // 모든 말이 도착했으면 게임 끝냄
-                for(int i = 0; i < 5; i++)
-                {
-                    horse[i] = new Horse();
-                    horse[i].Name = "말" + 'i';
-                }
 
-                for(int i = 0; i<5; i++)
+                for (int i = 0; i<5; i++)
                 {
-                    random = new Random();
-                    int tmpSpeed = random.Next(minSpeed, maxSpeed + 1);
-                    horse[i].Run(tmpSpeed);
-                    if (horse[i].TotalDistance > goalPos)
+                    if(!horse[i].IsFinished)
                     {
-                        name[i] = horse[i].Name;
+                        random = new Random();
+                        int tmpSpeed = random.Next(minSpeed, maxSpeed + 1);
+                        horse[i].Run(tmpSpeed);
+                        Console.WriteLine(horse[i].TotalDistance);
+                        if (horse[i].TotalDistance >= goalPos)
+                        {
+                            name[j++] = horse[i].Name;
+                            horse[i].IsFinished = true;
+                        }
                     }
+                }
+                if (j>= 5)
+                {
+                    break;
                 }
 
                 Thread.Sleep(1000);
+                sec++;
             }
+            Console.WriteLine("=================경주 끝================");
 
-            for(int i = 0; i<5; i++)
+            for (int i = 0; i<5; i++)
             {
                 Console.WriteLine(name[i]);
             }
---
+++ CSharp/Structure/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+
+//구조체 사용자 정의 자료형
+//멤버들을 캡슐화하는 자료형
+//멤버 변수, 프로퍼티, 함수 ..
+//
+//선언 형태
+//struct 이름
+//{
+//    멤버들 선언/정의
+//}
+
+namespace Structure
+{
+    public struct Stats
+    {
+        //프로퍼티
+        //getter/setter (get 접근자/ set 접근자)를 구현할 수 있는 속성
+        public int STR
+        {
+            get
+            {
+                return _str;
+            }
+            set
+            {
+                if (value < 0)
+                    return;
+                _str = value;
+            }
+        }
+        public static Stats Zero
+        {
+            get
+            {
+                return new Stats(0,0,0,0);
+            }
+            private set
+            {
+
+            }
+        }
+        public static Stats Default => new Stats(10, 10, 10, 10);
+
+        private int _str;
+        private int _dex;
+        private int _int;
+        private int _luk;
+
+        //오버로드(overload)
+        //aoroqustnrk ekfmaustj ehddlfgks dlfmadml gkatnfmf wjddmlgkf tn dlTsms rlsmd
+
+        //생성자 오버로딩
+        //
+        public Stats(int STR, int DEX, int INT, int LUK)
+        {
+            this._str = STR;
+            this._dex = DEX;
+            this._int = INT;
+            this._luk = LUK;
+        }
+
+        public static Stats GetDefault()
+        {
+            return new Stats(10, 10, 10, 10);
+        }
+        public static Stats GetZero()
+        {
+            return new Stats(0, 0, 0, 0); 
+        }
+        public void SetSTR(int value)
+        {
+            if (value < 0)
+                return;
+            this._str = value;
+        }
+        public int GetSTR()
+        {
+            return this._str;
+        }
+        public void SetDEX(int value)
+        {
+            if (value < 0)
+                return;
+            this._dex = value;
+        }
+        public int GetDEX()
+        {
+            return this._dex;
+        }
+        public void SetINT(int value)
+        {
+            if (value < 0)
+                return;
+            this._int = value;
+        }
+        public int GetINT()
+        {
+            return this._int;
+        }
+        public void SetLUK(int value)
+        {
+            if (value < 0)
+                return;
+            this._luk = value;
+        }
+        public int GetLUK()
+        {
+            return this._luk;
+        }
+
+
+        public int GetCombatPower()
+        {
+            return _str + _dex + _int + _luk;
+        }
+
+        public int GetCombatPower(int gain)
+        {
+            return GetCombatPower() * gain;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //클래스는 참조타입
+            Player player = new Player();
+            player.STR = 10;
+            player.DEX = 10;
+            player.INT = 10;
+            player.LUK = 10;
+
+            //구조체는 값타입
+            PlayerPlayer playerplayer = new PlayerPlayer();
+            playerplayer.STR = 10;
+            playerplayer .DEX = 10;
+            playerplayer .INT = 10;
+            playerplayer .LUK = 10;
+
+            //그러면 언제 클래스틑 쓰고 언제 구조체를 쓰냐?
+            //1.멤버들의 크기 총합이 16byte 이상일때는 일반적으로 클래스를 쓴다
+
+            //참조타입으로 값을 읽고 쓰는것보다 값타입을 읽고 쓰는게 빠르다.
+            //하지만 16byte를 초과할때는 레지스터가 두번 이상 값을 읽어야 하기 때문에
+            //값타입이어도 참조타입보다 느려진다.
+            
+            //2.값을 쓰고 읽는 것이 빈번하게 일어날 경우 (ex. 함수의 인자로 넘겨주는 횟수가 잦을 경우)
+            //구조체를 사용하는 것이 효율적이다.
+
+
+            Enemy enemy = new Enemy();
+            enemy.stats.SetSTR(10);
+            enemy.stats.SetDEX(10);
+            enemy.stats.SetINT(10);
+            enemy.stats.SetLUK(10);
+            enemy.stats.STR = 10;
+            Console.WriteLine(enemy.stats.STR);
+            enemy.stats = new Stats(10, 10, 10, 10);
+            enemy.stats = Stats.GetDefault();
+            Console.WriteLine("적의 전투력이" + enemy.stats.GetCombatPower() + "입니다");
+        }
+    }
+    public class Player
+    {
+        public int STR;
+        public int DEX;
+        public int INT;
+        public int LUK;
+    }
+    public struct PlayerPlayer
+    {
+        public int STR;
+        public int DEX;
+        public int INT;
+        public int LUK;
+    }
+    public class Enemy
+    {
+        public Stats stats;
+    }
+}
