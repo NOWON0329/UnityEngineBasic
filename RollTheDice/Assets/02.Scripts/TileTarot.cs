a71677dc3d936e1f160f4826dd0327ace8aa2b97
NOWON0329 <savdb032945@gmail.com>
2023-02-13 21:49:26 +0900

    RollTheDice - N

---
+++ RollTheDice/Assets/02.Scripts/TileTarot.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTarot : MonoBehaviour
+{
+    [SerializeField] private List<TarotCard> _cards;
+
+    public override void OnHere()
+    {
+
+    }
+}
